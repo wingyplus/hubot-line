b58acbaef797fe61d0831cc272e8635ac7d549f1
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-08-20 23:02:49 +0700

    Add webhook to receive message from line
    This changes add 2 environment variables to uses with line webhook:
    
    - HUBOT_LINE_CHANNEL_ACCESS_TOKEN - it's signature that LINE called.
    - HUBOT_LINE_CHANNEL_SECRET

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const Line = require("./src/bot");
 
-module.exports.use = (robot) => new Line(robot);
+const config = {
+  channelAccessToken: process.env.HUBOT_LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.HUBOT_LINE_CHANNEL_SECRET,
+};
+
+module.exports.use = (robot) => new Line(robot, config);
--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,33 @@
 const Adapter = require.main.require('hubot/src/adapter');
+const { validateSignature } = require('@line/bot-sdk');
 
 /**
  * Line bot adapter for Hubot.
  */
 class Line extends Adapter {
 
-  constructor(robot) {
+  constructor(robot, config) {
     super(robot);
     this.robot.logger.info('Constructor');
+
+    this.config = config;
+    this.listenWebhook();
   }
 
   run() {
     this.robot.logger.info('Run');
     this.emit('connected');
+  }
+
+  listenWebhook() {
+    this.robot.router.post('/hubot/webhook',  (req, res) => {
+      if (validateSignature(JSON.stringify(req.body), this.config.channelSecret, this.config.channelAccessToken)) {
+        res.send(401, 'invalid signature');
+        return;
+      }
+      console.log(req.body.events);
+      res.sendStatus(200);
+    });
   }
 }
 
